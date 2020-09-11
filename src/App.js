eb425102aae2f4002c64cadf41f498f0285fe1d3
theeugeniakim <elk303@nyu.edu>
2020-09-11 15:32:00 -0400

    nav bar functionatliy
    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComponent from './Components/NavBarComponent'
 
 function App() {
+  const [navBar, setNavBar] = useState("about")
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBarComponent setNavBar={setNavBar} />
+      Nav Bar Selected: {navBar}
+      {/* <AppContainer navBar={navBar} /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/Components/NavBarComponent.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+import  {Navbar, Nav}  from 'react-bootstrap'
+
+function NavBarComponent (props){
+  function handleAboutClick() {
+    props.setNavBar("about")
+  }
+
+  function handleEvolutionClick() {
+    props.setNavBar("evolution")
+  }
+
+  function handleAdvancedSearchClick(){
+    props.setNavBar("advanced-search")
+  }
+
+  function handleCountryClick(){
+    props.setNavBar("gender-by-country")
+  }
+
+  function handleDOBClick(){
+    props.setNavBar("gender-by-DOB")
+  }
+
+  function handleLanguageClick(){
+    props.setNavBar("language")
+  }
+
+  function handleDocumentationClick() {
+    props.setNavBar("documentation")
+  }
+  return (
+    <React.Fragment>
+        <Navbar bg="light" variant="light" sticky="top">
+          <Navbar.Brand href="#About">
+            Humaniki 
+          </Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link onClick={handleAboutClick} >About</Nav.Link>
+            <Nav.Link onClick={handleEvolutionClick}>Evolution</Nav.Link>
+            <Nav.Link onClick={handleAdvancedSearchClick}>Advanced Search</Nav.Link>
+            <Nav.Link onClick={handleCountryClick}>Gender By Country</Nav.Link>
+            <Nav.Link onClick={handleDOBClick}>Gender By Date of Birth</Nav.Link>
+            <Nav.Link onClick={handleLanguageClick}>Gender By Language</Nav.Link>
+            <Nav.Link onClick={handleDocumentationClick}>Documentation</Nav.Link>
+          </Nav>
+        </Navbar>
+    </React.Fragment>
+  )
+}
+
+export default NavBarComponent; 
