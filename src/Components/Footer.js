2f9b3d492a3f3f71c3e4c1a40fa336980cf5b107
theeugeniakim <elk303@nyu.edu>
2020-11-19 11:05:49 -0500

    navbar fixed to bottom

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import  {Navbar, Nav}  from 'react-bootstrap'
 
 function Footer(){
   return (
-    <Navbar sticky="bottom" className="Footer">
+    <Navbar fixed="bottom" className="Footer">
       Powered by a grant funded by the Wikimedia Foundation 
     </Navbar>
   )
