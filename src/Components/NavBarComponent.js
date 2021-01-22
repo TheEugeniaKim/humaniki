d18cbc0c09a63a524f3ab408cc783909c14c7c25
theeugeniakim <elk303@nyu.edu>
2021-01-22 11:33:36 -0500

    nav styling fix

--- src/Components/NavBarComponent.js
+++ src/Components/NavBarComponent.js
@@ -12,13 +12,15 @@ function NavBarComponent ({setNavBar}){
           <Navbar.Brand href="/" >
             <img className="nav-logo" src="./humaniki.png" alt="humaniki-logo"/>
           </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Nav className="mr-auto" >
             <Nav.Link href="/about">About</Nav.Link>
-              <NavDropdown title="Vizualizations" id="basic-nav-dropdown" >
+              <NavDropdown title="Vizualizations" id="collasible-nav-dropdown" >
                 <NavDropdown.Item href="/gender-by-country">Gender By Country</NavDropdown.Item>
                 <NavDropdown.Item href="/gender-by-dob" >Gender By Date of Birth</NavDropdown.Item>
                 <NavDropdown.Item href="/gender-by-language" >Gender By Language</NavDropdown.Item>
               </NavDropdown>
+            <Nav.Link  href="/advanced-search">Advanced Search</Nav.Link>
             <Nav.Link  href="/combine-search">Combine Search</Nav.Link>
             <Nav.Link  href="/faq">FAQ</Nav.Link>           
           </Nav>
