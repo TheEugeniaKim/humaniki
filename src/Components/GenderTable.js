4b213fc09c9b36ce29586896980d0fb62f9d240d
theeugeniakim <elk303@nyu.edu>
2021-01-25 13:49:11 -0500

    breakout modal component

--- src/Components/GenderTable.js
+++ src/Components/GenderTable.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
+import Modal from '../Components/Modal';
 import BootstrapTable from "react-bootstrap-table-next";
 import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
 import filterFactory, { afterFilter } from "react-bootstrap-table2-filter";
 import paginationFactory from "react-bootstrap-table2-paginator";
-import { toast } from 'react-toastify'
 
 function GenderTable({ tableColumns, tableArr }) {
   const [showExpandGenders, setShowExpandGenders] = useState(false);
@@ -20,61 +19,6 @@ function GenderTable({ tableColumns, tableArr }) {
     }
     setMyToggles(visibleColumnsDefault)
   }, [tableColumns])   
-
-  function handleMyColumnToggle(dataField){
-    let tempMyToggles = myToggles
-    const targetVisibility = !tempMyToggles[dataField]
-    tempMyToggles[dataField] = targetVisibility
-    setMyToggles(tempMyToggles)
-    toast(`Setting ${dataField} Column ${targetVisibility ? "Visible" : "Hidden"}`)
-  }
-
-  function MyVerticallyCenteredModal({
-    columns,
-    onColumnToggle,
-    toggles, 
-    show,
-    onHide,
-    newToggles
-  }) {
-    return (
-      <Modal
-        show={show}
-        onHide={onHide}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title-vcenter">
-            Columns: 
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {columns
-            .map((column) => ({
-              ...column,
-              toggle: newToggles[column.dataField],
-            }))
-            .map((column) => (
-              <button
-                type="button"
-                key={column.dataField}
-                className={`btn btn-warning ${newToggles[column.dataField] ? "active" : ""}`}
-                data-toggle="button"
-                aria-pressed={newToggles[column.dataField] ? "true" : "false"}
-                onClick={() => handleMyColumnToggle(column.dataField)}
-              >
-                {column.text}
-              </button>
-            ))}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={onHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    );
-  }
 
   function handleGenderExpandClick(event) {
     setShowExpandGenders(!showExpandGenders);
@@ -96,11 +40,12 @@ function GenderTable({ tableColumns, tableArr }) {
                 Other Genders Breakdown
               </Button> 
 
-              <MyVerticallyCenteredModal
+              <Modal
                 show={modalShow}
                 onHide={() => setModalShow(false)}
                 {...props.columnToggleProps}
                 newToggles={myToggles}
+                setMyToggles={setMyToggles}
               /> 
             </div>
 
