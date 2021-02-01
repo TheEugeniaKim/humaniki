eb1bb7d1460a219cd28a4f586edaf9695d419b21
Max <max@notconfusing.com>
2021-01-31 17:07:45 -0800

    fix empty gender table processing

--- src/Components/GenderTable.js
+++ src/Components/GenderTable.js
@@ -1,79 +1,81 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import Button from "react-bootstrap/Button";
 import ModalComponent from '../Components/ModalComponent';
 import BootstrapTable from "react-bootstrap-table-next";
 import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
-import filterFactory, { afterFilter } from "react-bootstrap-table2-filter";
+import filterFactory, {afterFilter} from "react-bootstrap-table2-filter";
 import paginationFactory from "react-bootstrap-table2-paginator";
 
-function GenderTable({ tableColumns, tableArr, keyField }) {
-  const [showExpandGenders, setShowExpandGenders] = useState(false);
-  const [modalShow, setModalShow] = useState(false)
-  const [myToggles, setMyToggles] = useState({"male": true})
+function GenderTable({tableColumns, tableArr, keyField}) {
+    const [showExpandGenders, setShowExpandGenders] = useState(false);
+    const [modalShow, setModalShow] = useState(false)
+    const [myToggles, setMyToggles] = useState({"male": true})
 
-  useEffect(() => {
-    if (!tableColumns) {
-     return 
-    } else {
-      const visibleColumnsDefault = {}
-      for (let colObj of tableColumns){
-        visibleColumnsDefault[colObj.dataField] = !colObj.hidden
-      }
-      setMyToggles(visibleColumnsDefault)
-    }
-  }, [tableColumns])   
-
-  function handleGenderExpandClick(event) {
-    setShowExpandGenders(!showExpandGenders);
-  }
-
-  if (tableArr && tableColumns){
-    return (
-      <ToolkitProvider
-        keyField={keyField}
-        data={tableArr}
-        columns={tableColumns}
-        columnToggle
-        exportCSV
-      >
-        {(props) => (
-          <div>
-            <div className="expand-genders">
-              <Button onClick={() => setModalShow(true)} >
-                Other Genders Breakdown
-              </Button> 
-
-              <ModalComponent
-                show={modalShow}
-                onHide={() => setModalShow(false)}
-                {...props.columnToggleProps}
-                newToggles={myToggles}
-                setMyToggles={setMyToggles}
-              /> 
-            </div>
-
-            {/* if myToggles is null don't pass in columnToggle props */}
-            { myToggles ? 
-              <BootstrapTable
-                {...props.baseProps}
-                columnToggle={{"toggles": myToggles}}
-                filter={filterFactory({ afterFilter })}
-                pagination={paginationFactory()}
-                striped
-                condensed
-              /> : 
-              null
+    useEffect(() => {
+        if (!tableColumns) {
+            return
+        } else {
+            const visibleColumnsDefault = {}
+            for (let colObj of tableColumns) {
+                visibleColumnsDefault[colObj.dataField] = !colObj.hidden
             }
-          </div>
-        )}
-      </ToolkitProvider>
-      
-    ) 
-  } else {
-    // don't display if there are no tableColumns
-    return null
-  }
+            setMyToggles(visibleColumnsDefault)
+        }
+    }, [tableColumns])
+
+    function handleGenderExpandClick(event) {
+        setShowExpandGenders(!showExpandGenders);
+    }
+
+    const tableElementsComplete = tableColumns && tableArr && tableColumns.length > 1 && tableArr.length > 0
+    // why does JS think the empty object have length 1???
+    if (!tableElementsComplete) {
+        // don't display if there are no tableColumns
+        return null
+    } else {
+        return (
+            <ToolkitProvider
+                keyField={keyField}
+                data={tableArr}
+                columns={tableColumns}
+                columnToggle
+                exportCSV
+            >
+                {(props) => (
+                    <div>
+                        <div className="expand-genders">
+                            <Button onClick={() => setModalShow(true)}>
+                                Other Genders Breakdown
+                            </Button>
+
+                            <ModalComponent
+                                show={modalShow}
+                                onHide={() => setModalShow(false)}
+                                {...props.columnToggleProps}
+                                newToggles={myToggles}
+                                setMyToggles={setMyToggles}
+                            />
+                        </div>
+
+                        {/* if myToggles is null don't pass in columnToggle props */}
+                        {myToggles ?
+                            <BootstrapTable
+                                {...props.baseProps}
+                                columnToggle={{"toggles": myToggles}}
+                                filter={filterFactory({afterFilter})}
+                                pagination={paginationFactory()}
+                                striped
+                                condensed
+                            /> :
+                            null
+                        }
+                    </div>
+                )}
+            </ToolkitProvider>
+
+        )
+    }
 }
 
 export default GenderTable;
