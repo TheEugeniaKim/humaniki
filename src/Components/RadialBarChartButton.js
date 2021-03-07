004a353fa4dd3908d4d933f0d5949cc6d7e29d14
theeugeniakim <elk303@nyu.edu>
2021-03-06 19:14:32 -0500

    radial bar cleaned up

--- src/Components/RadialBarChartButton.js
+++ src/Components/RadialBarChartButton.js
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import "../App.css";
 import "../Sk.css";
-// import * as d3 from 'd3'
 import { select, arc, pie, interpolate } from "d3";
 import useResizeObserver from './useResizeObserver';
 
@@ -38,7 +37,9 @@ function RadialBarChart({data}){
         .attr("alignment-baseline", "middle")
         .style("fill", "black")
         .style("font-size", 8)
-        .text(`${completePercentDisplay}% data Complete`)
+        .style("transform", `translate(-20px)`)
+        .style("transform", `translateY(60px)`)
+        .text(`${completePercentDisplay}% Complete`)
 
     }, [data,dimensions])
   
@@ -47,62 +48,6 @@ function RadialBarChart({data}){
       <svg ref={svgRef}></svg>
     </div>
   )
-
-
-  // const ref = useRef(null)
-  // const createPie = d3
-  //   .pie()
-  //   .value(data => data.value)
-  //   .sort(null)
-  // const createArc = d3
-  //   .arc()
-  //   .innerRadius(75)
-  //   .outerRadius(100)
-  // const colors = d3.scaleOrdinal(["#00BCA1","#BC8F00","#6200F8"])
- 
-  // useEffect(() => {
-  //   const data = createPie(props.data)
-  //   // const percentWomen = props.data.filter(obj => obj.label === "women")[0].value
-  //   const group = d3.select(ref.current)
-  //   const groupWithData = group.selectAll("g.arc").data(data)
-
-  //   groupWithData.exit().remove()
-
-  //   const groupWithUpdate = groupWithData
-  //     .enter()
-  //     .append("g")
-  //     .attr("class", "arc")
-    
-  //   const path = groupWithUpdate
-  //     .append("path")
-  //     .merge(groupWithData.select("path.arc"))
-    
-  //   path  
-  //     .attr("class", "arc")
-  //     .attr("d", createArc)
-  //     .attr("fill", (d,i) => colors(i))
-    
-  //   const text = groupWithUpdate
-  //     .append("text")
-  //     .merge(groupWithData.select("text"));
-  //   text
-  //     .attr("text-anchor", "middle")
-  //     .attr("alignment-baseline", "middle")
-  //     .style("fill", "black")
-  //     .style("font-size", 8)
-  //     // .text(d => `${percentWomen}% Women`)
-  // })
-
-  // return (
-  //   <div>
-  //     <svg className="button-svg" width={props.width} height={props.height}>
-  //       <g 
-  //         ref={ref}
-  //         transform={`translate(${props.outerRadius} ${props.outerRadius})`}
-  //       />
-  //     </svg>
-  //   </div>
-  // )
 }
 
 export default RadialBarChart;
